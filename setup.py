718827210e7288a039e13313ad108a65c76ed28b
Alvin Liang <ayliang@gmail.com>
2014-01-09 18:51:48 -0800

    change file extension, see what happens?

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     url='https://github.com/aliang/pokerhand-eval',
     license='Apache, see LICENSE.txt',
     description='A pure python poker hand evaluator for 5, 6, 7 cards',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
 )
